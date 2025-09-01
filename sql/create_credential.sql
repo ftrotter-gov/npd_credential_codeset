411b9ddfb6d8ace835d9443c96ef987a505977af
ftrotter-gov <frederick.trotter@cms.hhs.gov>
2025-09-01 18:12:12 -0400

    looks like it worked

--- sql/create_credential.sql
+++ sql/create_credential.sql
@@ -1,3 +1,18 @@
+-- Column explinations: 
+-- *  id - should be left alone, these were manually created to leave space between sections of the file
+-- *  credential_abbr - the credential abbreviation should already be set
+-- *  credential_name - the name of the credential spellout out, like "Medical Doctor" for M.D. and "Registered Nurse" for RN
+-- *  credentialing_organization_name - not every organization has a single source, but when it does list it here.
+-- *  credentialing_organization_url - the best url for that credential. Sometimes the best that can be gotten is the link to the credentialing organization home page.
+-- *  credential_description - a natural language of the credential and its purpose.
+-- *  is_multisource - lots of schools will grant an M.D. or an RN. These are multi-source credentials.
+-- *  is_clinical - there are plenty of credentials, like CPA or CEO that have nothing to do with clinical practice. For the purposes of the database, we consider Veterinarian as non-clinical.
+-- *  is_board_certification - should this credential be considered "board certified"
+-- *  is_credential_retired - sometimes a credentialling organization will stop issuing a credential to new clinicians, but allow previous credential holders to continue using the credential.
+-- *  created_at - should always be NULL so that the database can set
+-- *  updated_at - should always be NULL so that the database can set.
+
+
 CREATE TABLE dctnry.credential (
   id                          INT PRIMARY KEY,
   credential_abbr             TEXT        NOT NULL,  -- e.g., MD, DO, PhD
